5d167800836375a3c558005cd2d5d9d92dcbfbf4
Barry Closser <bmckwd@mst.edu>
2016-03-29 22:57:37 -0500

    Add Angular and get data from URL
    Added Angular and Font Awesome
    Loaded Members from URL into Table

---
+++ app.js
@@ -0,0 +1,21 @@
+//app.js
+angular.module('aquentChallenge', [])
+
+    .controller('appController', function ($scope, $http) {
+        $scope.sort = 'firstName';
+        $scope.sortReverse = false;
+        $scope.search = '';
+        $scope.members = [];
+        $scope.loadingMembers = true;
+
+        //load list of members from API
+        $http.get('http://private-a73e-aquentuxsociety.apiary-mock.com/members')
+        .success(function (data, status, headers, config) {
+            $scope.members = data;
+            $scope.loadingMembers = false;
+        })
+        .error(function (error, status, headers, config) {
+            console.log(status);
+            console.log("Error occured");
+        });
+    });
